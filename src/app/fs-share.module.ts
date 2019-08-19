c46188de0cb2acc26e2ea930ef94bc741e73cc96
Raymond Gigliotti <ray@firestitch.com>
2019-08-19 13:27:00 -0400

    removed directive from module

--- src/app/fs-share.module.ts
+++ src/app/fs-share.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClipboardModule } from 'ngx-clipboard';
 
-import { FsShareDirective } from './directives/share.directive';
 import { FsShareService } from './services/share.service';
 import { FsShareButtonComponent } from './components/share-button/share-button.component';
 import { DeviceDetectorModule } from 'ngx-device-detector';
